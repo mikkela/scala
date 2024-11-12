58d64ed712adaca8f677ac47850e184d857a4a05
Mikkel Arentoft <mikkel@mikadocs.com>
2024-11-12 20:35:03 +0100

    Created main loop

---
+++ kamin/build.sbt
@@ -0,0 +1,10 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "3.6.1"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "kamin"
+  )
+
+libraryDependencies += "org.jline" % "jline" % "3.27.1"
---
+++ kamin/src/main/scala/main.scala
@@ -0,0 +1,32 @@
+import org.jline.reader.{LineReader, LineReaderBuilder}
+import org.jline.terminal.TerminalBuilder
+
+def isBalanced(input: String): Boolean =
+  var stack = List[Char]()
+
+  for (char <- input)
+    char match
+      case '(' => stack = char :: stack // Push opening parenthesis to stack
+      case ')' =>
+        if (stack.isEmpty || stack.head != '(') return false // Unmatched closing parenthesis
+        stack = stack.tail // Pop the matching opening parenthesis
+      case _ => // Ignore other characters
+
+  stack.isEmpty // If stack is empty, all parentheses were balanced
+@main
+def main(): Unit = {
+  val terminal = TerminalBuilder.terminal()
+  val lineReader = LineReaderBuilder.builder().terminal(terminal).build()
+
+  var continue = true
+  while continue do
+    var input = lineReader.readLine("->")
+
+    if (input == "exit") continue = false
+    else
+      while !isBalanced(input) do
+        input = input + lineReader.readLine(">")
+
+      println("Entered: " + input)
+}
+
