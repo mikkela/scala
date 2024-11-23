3bc2456bfbf554aae91c02f1a68538fa50ae591e
Mikkel Arentoft <mikkel@mikadocs.com>
2024-11-23 11:04:12 +0100

    ADded VectorValue

--- kamin/src/main/scala/apl/value.scala
+++ kamin/src/main/scala/apl/value.scala
@@ -5,6 +5,20 @@ import kamin.{Reader, Value}
 import scala.annotation.targetName
 import scala.util.{Failure, Success, Try}
 
+case class VectorValue private(value: Vector[Int]) extends Value:
+  override def isTrue: Boolean = this != VectorValue.emptyVector
+  override def toString: String = value.mkString(" ") + "\n"
+
+
+object VectorValue:
+  def createVector(value: Vector[Int]): Either[String, VectorValue] =
+    if (value.isEmpty)
+      Left("Unable to create an empty Vector")
+    else
+      Right(VectorValue(value))
+
+  def emptyVector: VectorValue = VectorValue(Vector.empty)
+
 case class MatrixValue(value: Vector[Vector[Int]]) extends Value:
   require(
     value.nonEmpty && value.forall(_.length == value.head.length),
@@ -15,12 +29,12 @@ case class MatrixValue(value: Vector[Vector[Int]]) extends Value:
 object MatrixValue:
   def fromVector(vector: Vector[Int]): MatrixValue = MatrixValue(Vector(vector))
 
-trait MatrixValueReader extends Reader:
+trait VectorValueReader extends Reader:
   override def read(input: String): Either[String, Value] =
     if input.startsWith("'") && input.drop(1).trim.startsWith("(") && input.endsWith(")") then
       val content = input.drop(1).trim.drop(1).dropRight(1) // Remove `'` and parentheses
       Try(content.split("\\s+").map(_.toInt).toVector) match
-        case Success(value) => Right(MatrixValue.fromVector(value))
+        case Success(value) => VectorValue.createVector(value)
         case Failure(_) => super.read(input)
     else
       super.read(input)
